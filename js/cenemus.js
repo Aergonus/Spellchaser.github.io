a72a6b3be180c051bb9afd7e8e121b774b39ba5e
Spellchaser <Spellchaser@users.noreply.github.com>
2015-12-07 19:36:43 -0500

    Create cenemus.js

---
+++ js/cenemus.js
@@ -0,0 +1,19 @@
+var parameters = location.search.substring(1).split("&");
+var poll, user;
+
+$(document).ready(function() {
+  for (i in parameters) {
+    var temp = parameters[i].split("=");
+    switch(temp[0]) {
+      case 'p': poll = temp[1];
+          break;
+      case 'u': user = temp[1];
+          break;
+      default:
+          console.log("Invalid parameter.");
+    }
+  }
+  
+  console.log("Poll: " + p + ", User: " + u);
+});
+
