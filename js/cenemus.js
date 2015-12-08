522ad6794312c1e5d6044e1d767b18c8e0f972c8
Spellchaser <Spellchaser@users.noreply.github.com>
2015-12-07 22:25:35 -0500

    Save after class
    Eh... Redo Later
    
    Firebase has internal UUID generator and auth rules. Use their authentication. 
    Based on cookie or session -> check what is avail. Write security rules.

--- js/cenemus.js
+++ js/cenemus.js
@@ -1,7 +1,20 @@
+var Firebase = require("firebase");
 var parameters = location.search.substring(1).split("&");
 var poll, user;
+var hash = CryptoJS.MD5(poll);
+var callbacks = $.Callbacks();
+callbacks.add(load);
 
 $(document).ready(function() {
+  var UUID;
+  $.ajax({
+    type: 'GET',
+    dataType: 'text',
+    url: "http://cors.io/?u=https://www.uuidgenerator.net/api/version4",
+    success: function(response){
+      UUID = response;
+    }
+  });
   for (i in parameters) {
     var temp = parameters[i].split("=");
     switch(temp[0]) {
@@ -13,7 +26,25 @@ $(document).ready(function() {
           console.log("Invalid parameter.");
     }
   }
-  
+
   console.log("Poll: " + poll + ", User: " + user);
 });
 
+$( "#create" ).click(function() {
+
+  alert( "Handler for .click() called." );
+});
+
+function getUUID(p, u) {
+
+}
+
+function setUser(u) {
+  user = u;
+  $("input[name=u]").value = user;
+}
+
+function setPoll(p) {
+  poll = p;
+  $("input[name=p]").value = poll;
+}
